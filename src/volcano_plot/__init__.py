c5593bf918808b538a5350157de713bd63e50090
Martanto <martanto@live.com>
2026-04-22 21:32:17 +0200

    Update __init__.py

--- src/volcano_plot/__init__.py
+++ src/volcano_plot/__init__.py
@@ -2,10 +2,6 @@
 from importlib.metadata import version
 
 from volcano_plot.plot import add_inset, add_relief, simple_plot, create_figure
-from volcano_plot.config import load_config
-
-
-load_config()
 
 
 __version__ = version("volcano-plot")
@@ -25,5 +21,4 @@ __all__ = [
     "add_relief",
     "create_figure",
     "simple_plot",
-    "load_config",
 ]
